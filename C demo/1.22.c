6a346bf9bb269e3ada137357a4c02bb6190b024f
trupal95 <trupalkakadiya@gmail.com>
2024-09-04 12:01:04 +0530

    c demo pro

---
+++ C demo/1.22.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("---------\n");
+    printf("|       |\n");
+    printf("R       |\n");
+    printf("N       |\n");
+    printf("W       |\n");
+    printf("|       |\n");
+    printf("---------\n");
+
+printf("--------------\n");
+
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("**\n");
+    printf("*\n");
+    
+    printf("----------\n");
+    
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*        * *             *\n");
+    printf("*      *     *         *\n");
+    printf("*    *         *     *\n");
+    printf("*  *             * *\n");
+    printf("* *\n");
+    printf("*\n");
+    
+    return 0;
+}
